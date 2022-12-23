965c60da95291aec72eeaa3b5c7f5697db384044
DanJBower <bowerdanielj@gmail.com>
2022-12-23 13:40:40 +0000

    Created empty app

---
+++ src/PipelineController/PipelineController/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PipelineController;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/PipelineController/PipelineController/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PipelineController;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
