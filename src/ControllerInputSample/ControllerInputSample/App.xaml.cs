7f6c12d48b0df5c21c59f8da36e13479c7aa124f
DanJBower <bowerdanielj@gmail.com>
2024-03-02 16:00:47 +0000

    Created default project for simple controller view

---
+++ src/ControllerInputSample/ControllerInputSample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ControllerInputSample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
