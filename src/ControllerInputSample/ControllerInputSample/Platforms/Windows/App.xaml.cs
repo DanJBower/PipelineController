86b0454e9446305883e1ca83714e7b6064b72a0c
DanJBower <bowerdanielj@gmail.com>
2024-03-03 14:01:23 +0000

    Removed template comments

--- src/ControllerInputSample/ControllerInputSample/Platforms/Windows/App.xaml.cs
+++ src/ControllerInputSample/ControllerInputSample/Platforms/Windows/App.xaml.cs
@@ -1,14 +1,7 @@
 ﻿namespace ControllerInputSample.Platforms.Windows;
 
-/// <summary>
-/// Provides application-specific behavior to supplement the default Application class.
-/// </summary>
 public partial class App
 {
-    /// <summary>
-    /// Initializes the singleton application object.  This is the first line of authored code
-    /// executed, and as such is the logical equivalent of main() or WinMain().
-    /// </summary>
     public App()
     {
         InitializeComponent();
