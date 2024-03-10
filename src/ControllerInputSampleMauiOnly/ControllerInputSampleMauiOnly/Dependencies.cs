e177777fffe94875b4c5d5f80806abb3d384c403
DanJBower <bowerdanielj@gmail.com>
2024-03-10 20:30:17 +0000

    Going to maintain a separate DI file

---
+++ src/ControllerInputSampleMauiOnly/ControllerInputSampleMauiOnly/Dependencies.cs
@@ -0,0 +1,17 @@
+﻿using ControllerInputSampleMauiOnly.ViewModels;
+
+namespace ControllerInputSampleMauiOnly;
+
+public static class Dependencies
+{
+    public static MauiAppBuilder RegisterDependencies(this MauiAppBuilder mauiAppBuilder)
+    {
+        mauiAppBuilder.Services.Scan(scan => scan.FromCallingAssembly()
+            .AddClasses(classes => classes.AssignableTo<ViewModel>())
+            .AsSelf()
+            .WithTransientLifetime());
+
+        mauiAppBuilder.Services.AddTransient<Page, AppShell>();
+        return mauiAppBuilder;
+    }
+}
--- src/ControllerInputSampleMauiOnly/ControllerInputSampleMauiOnly/MauiProgram.cs
+++ src/ControllerInputSampleMauiOnly/ControllerInputSampleMauiOnly/MauiProgram.cs
@@ -1,6 +1,5 @@
 ﻿using CommunityToolkit.Maui;
 using CommunityToolkit.Maui.Markup;
-using ControllerInputSampleMauiOnly.ViewModels;
 using Microsoft.Extensions.Logging;
 
 namespace ControllerInputSampleMauiOnly;
@@ -26,18 +25,5 @@ public static class MauiProgram
 #endif
 
         return builder.Build();
-    }
-
-    private static MauiAppBuilder RegisterDependencies(this MauiAppBuilder mauiAppBuilder)
-    {
-        mauiAppBuilder.Services.Scan(scan => scan.FromCallingAssembly()
-            .AddClasses(classes => classes.AssignableTo<ViewModel>())
-            .AsSelf()
-            .WithTransientLifetime());
-		// Look into if I should use the C# markup from MauiCommunityToolkitMarkup or use XAML
-		// Error here: From CommunityToolkit having a add transient <TView, TViewModel> clashing with the normal add transient. May not use this, but need to investigate
-		// Is sample of how they do it in Repos\NotMine\Maui\samples\CommunityToolkit.Maui.Sample
-        mauiAppBuilder.Services.AddTransient<Page, AppShell>();
-        return mauiAppBuilder;
     }
 }
