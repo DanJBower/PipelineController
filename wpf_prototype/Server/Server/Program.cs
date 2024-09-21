0faef837574f8cecdd59f0e5411ab9d56575cad6
DanJBower <bowerdanielj@gmail.com>
2024-09-21 23:34:04 +0100

    Implemented simple MQTT server

--- wpf_prototype/Server/Server/Program.cs
+++ wpf_prototype/Server/Server/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Makaretu.Dns;
+using MQTTnet;
+using MQTTnet.Server;
+using ServerInfo;
+
+using var mqttServer = await StartServer();
+
+try
+{
+    using var serviceDiscovery = AdvertiseServer();
+    Console.WriteLine("Press Enter to exit.");
+    Console.ReadLine();
+}
+finally
+{
+    await mqttServer.StopAsync();
+}
+
+return;
+
+static async Task<MqttServer> StartServer()
+{
+    var mqttServerFactory = new MqttFactory();
+    var mqttServerOptions = new MqttServerOptionsBuilder()
+        .WithDefaultEndpoint()
+        .WithDefaultEndpointPort(ServerConstants.Port)
+        .Build();
+    var mqttServer = mqttServerFactory.CreateMqttServer(mqttServerOptions);
+    await mqttServer.StartAsync();
+    return mqttServer;
+}
+
+static ServiceDiscovery AdvertiseServer()
+{
+    var service = new ServiceProfile(ServerConstants.Name, ServerConstants.ServiceName, ServerConstants.Port);
+    var serviceDiscovery = new ServiceDiscovery();
+    serviceDiscovery.Advertise(service);
+    return serviceDiscovery;
+}
---
+++ wpf_prototype/common/ServerInfo/ServerConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerInfo;
+
+public static class ServerConstants
+{
+    public const string Name = "PrototypeMqttServer";
+    public const string ServiceName = "_mqtt._tcp";
+    public const int Port = 1883; // 1883 is the MQTT standard port
+}
