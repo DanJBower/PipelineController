adefb2da6a1f155962775b5c1f5a1e84de080b78
DanJBower <bowerdanielj@gmail.com>
2024-09-25 12:40:10 +0100

    Added way to just publish DLLs to quickly allow work with the scripts

--- wpf_prototype/publish.bat
+++ wpf_prototype/publish.bat
@@ -1,6 +1,4 @@
-dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "common\Controller\Controller.csproj" -o release -c release
-dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "common\ServerInfo\ServerInfo.csproj" -o release -c release
-dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "common\CommonClient\CommonClient.csproj" -o release -c release
+call publish_dlls.bat
 dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "Server\Server\Server.csproj" -o release -c release
 dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "ControllerPassthroughClient\ControllerPassthroughClient\ControllerPassthroughClient.csproj" -o release -c release
 dotnet publish /p:DebugType=None /p:DebugSymbols=false /p:PublishReadyToRunShowWarnings=true "ServerMessageMonitor\ServerMessageMonitor\ServerMessageMonitor.csproj" -o release -c release
