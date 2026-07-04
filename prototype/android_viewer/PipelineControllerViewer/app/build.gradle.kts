f5e2d1ee0428acc9dffdd989166c94983d67f8b3
DanJBower <bowerdanielj@gmail.com>
2026-07-04 18:41:27 +0100

    Updated version number of android app

--- prototype/android_viewer/PipelineControllerViewer/app/build.gradle.kts
+++ prototype/android_viewer/PipelineControllerViewer/app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 2
-        versionName = "0.0.2.0"
+        versionName = "0.0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
