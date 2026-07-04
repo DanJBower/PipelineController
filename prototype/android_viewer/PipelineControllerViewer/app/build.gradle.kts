7ef23ba15e3db32fc2b04278ad4a3bfd90c8416a
DanJBower <bowerdanielj@gmail.com>
2026-07-04 18:43:11 +0100

    Apparently there's 2 version numbers...

--- prototype/android_viewer/PipelineControllerViewer/app/build.gradle.kts
+++ prototype/android_viewer/PipelineControllerViewer/app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.danjbower.pipelinecontrollerviewer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
+        versionCode = 3
         versionName = "0.0.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
